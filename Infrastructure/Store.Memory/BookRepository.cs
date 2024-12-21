bb93a985569a9f4b42a35117d33581ff8c2f7150
DODOptaxa <dodoptaxa305@gmail.com>
2024-12-21 20:48:10 +0200

    Реализация интерфейса IBookRepository

---
+++ Infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Store.Memory;
+
+public class BookRepository : IBookRepository
+{
+    private readonly Book[] books = new[]
+    {
+        new Book(1, "Майн Кампф"),
+        new Book(2, "Art of programming"),
+        new Book(3, "C - this is cool")
+    };
+    
+    public Book[] GetAllByTitle(string title)
+    {
+        return books.Where(book => book.Title.Contains(title)).ToArray();
+    }
+}
--- domain/Store/IBookRepository.cs
+++ domain/Store/IBookRepository.cs
@@ -9,6 +9,6 @@ namespace Store
     public interface IBookRepository
     {
 
-        Book[] GetAllByTitle(string title);
+        ICollection<Book> GetAllByTitle(string title);
     }
 }
