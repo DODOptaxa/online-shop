9eb28d8058b8208c699cd6761e870f8910c33591
DODOptaxa <dodoptaxa305@gmail.com>
2025-01-06 11:34:25 +0200

    Реализация страницы заказа и перенос функционала корзины
    *   Удален `CartController`, его методы перемещены в новый `OrderController`.
    *   Добавлен `OrderViewModel` и `OrderItemViewModel` для отображения деталей заказа.
    *   Внедрен `IBookRepository.GetAllByIds` для оптимизированного получения данных о товарах.
    *   Разработана страница `/Order` для просмотра и управления содержимым корзины.
    *   Обновлен дизайн элементов корзины в UI, включая навигационную панель и кнопку добавления.

---
+++ Store.Web/Models/OrderViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Store.Web.Models
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+
+        public OrderItemViewModel[] Items { get; set; } = new OrderItemViewModel[0];
+
+        public uint TotalCount { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
