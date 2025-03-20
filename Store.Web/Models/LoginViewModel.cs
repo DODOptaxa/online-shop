1caafdc87726aa9c1e80a673087322c78a8c5aa9
DODOptaxa <dodoptaxa305@gmail.com>
2025-03-20 18:11:15 +0200

    Страница регистрации и логина

---
+++ Store.Web/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store.Web.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Пуста комірка")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Пароль\" є обов'язковим")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; } // Опція "Запам'ятати мене"
+    }
+}
---
+++ Store.Web/Models/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store.Web.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage ="Пуста комірка")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage ="Довжина паролю від 6 до 100 символів")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Паролі не співпадають")]
+        [Required(ErrorMessage = "Пуста комірка")]
+        public string ConfirmPassword { get; set; }
+        [Required(ErrorMessage = "Пуста комірка")]
+        public string Name { get; set; }
+    }
+}
